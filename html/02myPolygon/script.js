af7604b160b08e1986d2cbf11332520d271c655f
GodlyHamster <28106@ma-web.nl>
2020-04-11 13:54:53 +0200

    Finally finished Draggable point and let's get linear

---
+++ html/02myPolygon/script.js
@@ -0,0 +1,30 @@
+const canvas = document.getElementById('canvas');
+const context = canvas.getContext('2d');
+
+const width = window.innerWidth;
+const height = window.innerHeight;
+
+canvas.width = width;
+canvas.height = height;
+
+// begin hier met jouw code voor deze opdracht
+
+let points = [];
+
+for (let i = 0; i<5;i++){
+  let x = getRandomInt(0, width);
+  let y = getRandomInt(0, height);
+  let color = "rgba("+getRandomInt(0,255)+","+getRandomInt(0,255)+","+getRandomInt(0,255)+","+getRandomInt(1,10)/10+")";
+  let myPoint = new Polypoint(x,y,20,color,);
+  points.push(myPoint);
+}
+
+
+console.log(points);
+function draw() {
+  context.clearRect(0,0,width,height);
+  for (let i = 0; i<points.length;i++){
+    points[i].draw(context)
+  }
+}
+
